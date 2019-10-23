cefa336db1f8d345a3a37ac90a11a3cf04c2387d
Yuriy Ulyanets <yuriy.ulyanets@namecheap.com>
2019-10-23 18:05:33 +0300

    Added test download. Not working so far

--- scripts/index-controller.js
+++ scripts/index-controller.js
@@ -7,17 +7,10 @@ var indexController = {
         spinner: null
     },
     init: function(config) {
-
         this.data.config = config;
         this.uiElements.downloadButton = $('#btn-download-resume');
-        this.uiElements.downloadButton.click(function (e) {
-            var button = $(this);
-            button.attr("disabled", true);  
 
-            setTimeout(function(){
-                button.attr("disabled", false);
-              }, 5000);
-        });
+        this.wireupEvents();
     },
     getContentDispositionFileName: function(disposition, defaultFileName) {
         var filename = defaultFileName;
@@ -29,5 +22,50 @@ var indexController = {
             }
         }
         return filename;
+    },
+    wireupEvents: function(){
+
+        var that = this;
+
+        this.uiElements.downloadButton.click(function (e) {
+            var button = $(this);
+            button.attr("disabled", true);
+            //document.body.style.cursor = 'wait';
+
+            var requestUrl = that.data.config.apiBaseUrl + '?cmd=requestDownload';
+
+            $.ajax({
+                url: requestUrl,
+                type: 'POST',
+                data: null,
+                processData: false,
+                contentType: false,
+            }).done(function (response, status, xhr) {
+                
+                console.log(status);
+
+                var contentType = xhr.getResponseHeader('Content-Type');
+                var contentDisposition = xhr.getResponseHeader('Content-Disposition');
+
+                var fileName = that.getContentDispositionFileName(contentDisposition, 'Resume.doc');
+
+                var blob = new Blob([response], { type: contentType });
+                var link = document.createElement('a');
+                link.href = window.URL.createObjectURL(blob);
+                link.download = fileName;
+
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+
+                //document.body.style.cursor = 'default';
+                button.attr("disabled", false);
+
+            }).fail(function (response) {
+                
+                button.attr("disabled", false);
+                //document.body.style.cursor = 'default';
+            })
+        });
     }
 };
